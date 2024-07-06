f10f79545a726d1ea90e2a0de2aa07c2785cf0d6
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-06 07:36:33 +0200

    Creating DTOs inside Accounts Microservice

---
+++ src/main/java/com/dgmf/dto/CustomerDto.java
@@ -0,0 +1,13 @@
+package com.dgmf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class CustomerDto {
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
---
+++ src/main/java/com/dgmf/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.dgmf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class ResponseDto {
+    private String statusCode;
+    private String statusMsg;
+}
