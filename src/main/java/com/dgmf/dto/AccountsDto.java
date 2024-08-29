c7dd691220eed88b37acb49461fa489a11412176
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-29 09:42:19 +0200

    Adding Validation on AccountsDto and CustomerDto DTO Classes

--- src/main/java/com/dgmf/dto/AccountsDto.java
+++ src/main/java/com/dgmf/dto/AccountsDto.java
@@ -1,5 +1,7 @@
 package com.dgmf.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,7 +9,11 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class AccountsDto {
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
+    @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 }
--- src/main/java/com/dgmf/dto/CustomerDto.java
+++ src/main/java/com/dgmf/dto/CustomerDto.java
@@ -1,5 +1,9 @@
 package com.dgmf.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,8 +11,13 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class CustomerDto {
+    @NotEmpty(message = "Name Can Not Be Null or Empty")
+    @Size(min = 5, max = 30, message = "Length of Customer Name Should Be Between 5 and 30")
     private String name;
+    @NotEmpty(message = "Email Address Can Not Be Null or Empty")
+    @Email(message = "Email Address Should Be a Valid Value")
     private String email;
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number Must Be 10 Digits")
     private String mobileNumber;
     private AccountsDto accountsDto;
 }
